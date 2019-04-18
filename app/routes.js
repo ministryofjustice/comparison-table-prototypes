b92e8737685f7498fb0c83505322e42e05e7b484
Beverley Newing <bnewing92@hotmail.co.uk>
2019-04-18 10:47:38 +0100

    Update titles of options and add versioning to them

--- app/routes.js
+++ app/routes.js
@@ -17,12 +17,16 @@ router.get('/table-four', function (req, res) {
   res.render('table-four');
 });
 
-router.get('/table-one-v2', function (req, res) {
-  res.render('table-one-v2');
+router.get('/table-five', function (req, res) {
+  res.render('table-five');
 });
 
-router.get('/table-four-v2', function (req, res) {
-  res.render('table-four-v2');
+router.get('/table-six', function (req, res) {
+  res.render('table-six');
+});
+
+router.get('/table-seven', function (req, res) {
+  res.render('table-seven');
 });
 
 
